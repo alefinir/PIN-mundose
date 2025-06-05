143c83baa4e283a2e6ce3c9484d717538761304f
alefinir <antonio.lefinir@gmail.com>
2025-06-04 23:27:43 -0300

    Update create_apache.sh

--- create_apache.sh
+++ create_apache.sh
@@ -4,5 +4,5 @@ sudo yum install -y httpd.x86_64
 sudo systemctl enable httpd --now
 sudo yum install git -y
 sudo yum install rsync -y
-cd /home/ec2-user/tmp/; sudo git clone https://github.com/alefinir/static-site.git
+mkdir /home/ec2-user/tmp/; cd /home/ec2-user/tmp/; sudo git clone https://github.com/alefinir/static-site.git
 sudo rsync -av /home/ec2-user/tmp/static-site/ /var/www/html
