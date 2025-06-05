4f5c01223bfc90a51f2f3ff3408bab132152b3ce
alefinir <antonio.lefinir@gmail.com>
2025-06-04 22:12:46 -0300

    update script

--- create_apache.sh
+++ create_apache.sh
@@ -2,4 +2,6 @@
 sudo yum update -y
 sudo yum install -y httpd.x86_64
 sudo systemctl enable httpd --now
-sudo echo "<h1>Hello World From MUNDOS-E</h1>" > /var/www/html/index.html
+sudo yum install git
+#sudo echo "<h1>Hello World From MUNDOS-E</h1>" > /var/www/html/index.
+cd /var/www/html/index.html; git clone https://github.com/alefinir/static-site.git
