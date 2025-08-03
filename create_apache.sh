7dc60a70c741532d3311463b48d5b74b5205c823
tony <antonio.lefinir@gmail.com>
2025-08-02 21:55:29 -0300

    feat: configuration terraform backend // configuring eks cluster // app code and image

--- create_apache.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-sudo yum update -y
-sudo yum install -y httpd.x86_64
-sudo systemctl enable httpd --now
-sudo yum install git -y
-sudo yum install rsync -y
-mkdir /home/ec2-user/tmp/; cd /home/ec2-user/tmp/; sudo git clone https://github.com/alefinir/static-site.git
-sudo rsync -av /home/ec2-user/tmp/static-site/ /var/www/html
