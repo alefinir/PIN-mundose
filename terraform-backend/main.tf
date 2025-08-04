1d47fa4efa1e1e7e62a38d34c049c4eb6c184a27
tony <antonio.lefinir@gmail.com>
2025-08-04 11:15:26 -0300

    fix: terraform format

--- terraform-backend/main.tf
+++ terraform-backend/main.tf
@@ -61,7 +61,7 @@ resource "aws_vpc" "eks_vpc" {
 resource "aws_subnet" "public_subnet_a" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a" 
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -73,7 +73,7 @@ resource "aws_subnet" "public_subnet_a" {
 resource "aws_subnet" "public_subnet_b" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b" 
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
